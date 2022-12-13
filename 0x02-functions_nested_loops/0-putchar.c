4302f1d1a2703760294dff6f9fcedb61714b0351
Atilola Oluwatobiloba <tobiatilola@gmail.com>
2022-12-13 06:22:05 -0800

    o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - Print the word "_putchar"
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(p[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the alphabet in lowercase.
- * Return: void.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
+
 void print_alphabet(void)
 {
-	int i;
+	char ch;
 
-	for (i = 97; i < 123; i++)
+	ch = 'a';
+
+	while (ch <= 'z')
 	{
-		_putchar(i);
+		_putchar(ch);
+		ch++;
 	}
 	_putchar('\n');
 }
