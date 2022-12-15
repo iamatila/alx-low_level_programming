01a969ed81b6bb3b34ccbdebdcb19d609573affe
Atilola Oluwatobiloba <tobiatilola@gmail.com>
2022-12-15 13:53:56 -0800

    l

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,6 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-
 	/*while (i < 10)*/
 
 	/*{*/
@@ -19,7 +18,7 @@ int main(void)
 	/*putchar(i);*/
 
 	/*}*/
-
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
